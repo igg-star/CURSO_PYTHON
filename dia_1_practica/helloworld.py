e8d8f61a9c54eac96396b202bfc206246717187c
Ignacio Mendez <mndzigg@gmail.com>
2026-02-02 23:33:18 -0600

    termine el lvl 3 day 1

---
+++ dia_1_practica/helloworld.py
@@ -0,0 +1,47 @@
+# EJERCICIO NIVEL 2
+# Crea una carpeta llamada day_1 dentro de la carpeta 30DaysOfPython.
+# Dentro de ella, crea el archivo de Python helloworld.py 
+# y repite las preguntas 1, 2, 3 y 4.
+# Recuerda usar print() cuando trabajes con un archivo de Python.
+
+# realice las siguientes operaciones.
+# Los operandos son 3 y 4.
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# Escribir cadenas en la consola interactiva de Python.
+# Las cadenas son las siguientes
+print('ignacio')
+print('mendez')
+print('mexico')
+print('estoy disfrutando de mi aprendizaje')
+
+# Compruebe los tipos de datos de los siguientes datos
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Ignacio', 'python', 'finlandia']))
+print(type('ignacio'))
+print(type('Mendez'))
+print(type('Mexico'))
+
+# LVL 3
+# Escriba un ejemplo para diferentes tipos de datos de Python, 
+# como Número (Entero, Flotante, Complejo), 
+# Cadena, Booleano, Lista, Tupla, Conjunto y Diccionario.
+print(type(10))                                  # Entero
+print(type(9.8))                                 # Flotante
+print(type(3.14))                                # Flotante
+print(type(4 - 4j))                              # Complejo
+print(type("Ignacio"))                           # Cadena
+print(type(True))                                # Booleano
+print(type([1, 2, 3]))                           # Lista
+print(type((1, 2, 3)))                           # Tupla (Se usan paréntesis)
+print(type({1, 2, 3}))                           # Conjunto (Se usan llaves)
+print(type({'name':'Ignacio'}))                  # Diccionario (Clave:Valor)
--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,9 @@
-# Hola mundo
-print("Hola python")
+# Hola mundo curso
+
+print(2 + 3)              # Suma
+print(3 - 1)              # Resta
+print(4 * 2)              # Multiplicación 
+print(8 / 2)              # División
+print(5 // 2)             # División entera
+print(5 % 2)              # Módulo
+print(2 ** 3)             # Exponenciación
